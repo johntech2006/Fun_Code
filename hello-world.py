060234be9536eba097f5106a6ed08b2041cf64f6
TheRealAlaya <87546349+TheRealAlaya@users.noreply.github.com>
2021-07-16 15:21:53 -0700

    Update hello-world.py
    adding favorite number

--- hello-world.py
+++ hello-world.py
@@ -10,39 +10,57 @@ import time
 # Arguments passed
 #print("\nName of Python script:", sys.argv[0])
 
-color1 = str(sys.argv[1])
+#color1 = str(sys.argv[1])
 
 print("\n" + "\n" + "\n" + "\n" + "\n")
 
+time.sleep(1) # Sleep for 1 seconds
 print("Hello Alaya's World!")
 
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(1) # Sleep for 1 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
-time.sleep(2) # Sleep for 2 seconds
 
+time.sleep(1) # Sleep for 1 seconds
+color1 = input("What is your favorite color?")
+
+time.sleep(1) # Sleep for 1 seconds
+print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+
+time.sleep(1) # Sleep for 1 seconds
 print("Your favorite color is " + color1)
 
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(1) # Sleep for 1 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
-time.sleep(2) # Sleep for 2 seconds
 
+time.sleep(1) # Sleep for 1 seconds
 color2 = input("What is your second favorite color?")
 
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(1) # Sleep for 1 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
-time.sleep(2) # Sleep for 2 seconds
 
+time.sleep(1) # Sleep for 1 seconds
 print("Your second favorite color is " + color2)
 
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(1) # Sleep for 1 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
-time.sleep(2) # Sleep for 2 seconds
 
+time.sleep(1) # Sleep for 1 seconds
 print("I like "  + color2 + " as well!!!")
-time.sleep(2) # Sleep for 2 seconds
 
+time.sleep(1) # Sleep for 1 seconds
+number1 = input("What is your favorite number?")
+
+time.sleep(1) # Sleep for 1 seconds
+print("Your second favorite number is " + number1)
+
+number2 = int(number1) + 5
+
+time.sleep(1) # Sleep for 1 seconds
+print("I like "  + number2 + " !!!")
+
+time.sleep(1) # Sleep for 1 seconds
 print(":)\n" + ";)\n" + ":D\n" + ":)\n" + ";)\n" + ":D")
 
-time.sleep(2) # Sleep for 2 seconds
+time.sleep(1) # Sleep for 1 seconds
 print("Byeee!!!!")
 print("\n" + "\n" + "\n" + "\n" + "\n")
