0119e38fc1e46ea8a39e75587e4725b4ec6bf942
johntech2006 <johntech2006@gmail.com>
2021-07-16 14:53:19 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -12,22 +12,21 @@ import sys
 color1 = str(sys.argv[1])
 
 print("Hello Alaya's World!")
-print(".\n")
 
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
 
 print("Your favorite color is " + color1)
 
-#print("." \n + "." \n + "." \n + "." \n + "." \n)
+print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
 
 color2 = input("What is your second favorite color?")
 
-#print("." \n + "." \n + "." \n + "." \n + "." \n)
+print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
 
 print("Your second favorite color is " + color2)
 
-#print("." \n + "." \n + "." \n + "." \n + "." \n)
+print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
 
 print("I like "  + color2 + " as well!!!")
 
-#print("." \n + ";)" \n + ":)" \n + ";)" \n + ":)" \n)
+print(":)\n" + ";)\n" + ":D\n" + ":)\n" + ";)\n" + ":D")
