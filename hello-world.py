09dc0e94524a8fe30c7e025ff3c808c39147c5c1
johntech2006 <johntech2006@gmail.com>
2021-07-16 13:55:56 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -9,11 +9,14 @@ print("Total arguments passed:", n)
 # Arguments passed
 print("\nName of Python script:", sys.argv[0])
 
+color = sys.argv[1]
+
 print("Hello Alaya's World!")
 
-print("Your favorite color is", sys.argv[1], "!!!")
+print("Your favorite color is", color"!!!")
 
-text = input("What is your second favorite color?")
+color2 = input("What is your second favorite color?")
 
-print("Your second favorite color is", text)
-print("I like", text, "as well!!! ;)")
+print("Your second favorite color is", color2)
+
+print("I like", color2, "as well!!! ;)")
