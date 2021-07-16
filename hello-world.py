0f8ebd00dc747ec70561f58f41b02ac8fd8a57b2
johntech2006 <johntech2006@gmail.com>
2021-07-16 14:25:23 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -13,10 +13,10 @@ color1 = sys.argv[1]
 
 print("Hello Alaya's World!")
 
-print("Your favorite color is" + color1)
+print("Your favorite color is " + color1)
 
 color2 = input("What is your second favorite color?")
 
 print("Your second favorite color is " + color2)
 
-print("I like"  + color2 + "as well!!! ;)")
+print("I like "  + color2 + " as well!!! ;)")
