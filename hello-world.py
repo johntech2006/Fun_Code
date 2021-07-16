fd4fdb9f67ad426ab8b311206b8eccc848548ec0
johntech2006 <johntech2006@gmail.com>
2021-07-16 13:43:48 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+ 
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+
 print("Hello Alaya's World!")
+
+print("Your favorite color is", sys.argv[1], "!!!")
+
+text = input("What is your second favorite color?")
+
+print("Your second favorite color is", text)
+print("I like", text, "as well!!! ;)")
