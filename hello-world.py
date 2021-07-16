530d505b6ac4dc1eefafd87342a4f9476c0d3f5a
johntech2006 <johntech2006@gmail.com>
2021-07-16 14:58:36 -0700

    Update hello-world.py

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import time
  
 # total arguments
 #n = len(sys.argv)
@@ -13,20 +14,32 @@ color1 = str(sys.argv[1])
 
 print("Hello Alaya's World!")
 
+time.sleep(2) # Sleep for 2 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+time.sleep(2) # Sleep for 2 seconds
 
 print("Your favorite color is " + color1)
 
+time.sleep(2) # Sleep for 2 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+time.sleep(2) # Sleep for 2 seconds
 
 color2 = input("What is your second favorite color?")
 
+time.sleep(2) # Sleep for 2 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+time.sleep(2) # Sleep for 2 seconds
 
 print("Your second favorite color is " + color2)
 
+time.sleep(2) # Sleep for 2 seconds
 print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+time.sleep(2) # Sleep for 2 seconds
 
 print("I like "  + color2 + " as well!!!")
+time.sleep(2) # Sleep for 2 seconds
 
 print(":)\n" + ";)\n" + ":D\n" + ":)\n" + ";)\n" + ":D")
+
+time.sleep(2) # Sleep for 2 seconds
+print("Byeee!!!!")
