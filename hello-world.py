cbd4caf28935a282605841e59d56e4679d45baba
johntech2006 <johntech2006@gmail.com>
2021-07-16 18:58:35 -0700

    Update hello-world.py - add wait_f1 function
    @TheRealAlaya I am adding functions to make the code cleaner and shorter
    
    the new function is wait_f1()

--- hello-world.py
+++ hello-world.py
@@ -12,82 +12,67 @@ import time
 
 #color1 = str(sys.argv[1])
 
+def wait_f1():
+  time.sleep(1) # Sleep for 1 seconds
+  print(".\n")
+  time.sleep(1) # Sleep for 1 seconds
+  print(".\n")
+  time.sleep(1) # Sleep for 1 seconds
+  
 print("\n" + "\n" + "\n" + "\n" + "\n")
 
 time.sleep(1) # Sleep for 1 seconds
+
 print("Hello Alaya's World!")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 color1 = input("What is your favorite color?")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("Your favorite color is " + color1)
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 color2 = input("What is your second favorite color?")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("Your second favorite color is " + color2)
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("I like "  + color2 + " as well!!!")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 number1 = input("What is your favorite number?")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("Your favorite number is " + number1)
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 number2 = input("What is your second favorite number?")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("Your second favorite number is " + number2)
 
 number3 = int(number1) + int(number2)
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("I like "  + str(number3) + " !!!")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print(":)\n" + ";)\n" + ":D\n" + ":)\n" + ";)\n" + ":D")
 
-time.sleep(1) # Sleep for 1 seconds
-print(".\n" + ". \n" + ".\n" + ". \n" + ".\n")
+wait_f1()       # invoke wait_f1 function
 
-time.sleep(1) # Sleep for 1 seconds
 print("Byeee!!!!")
+
 print("\n" + "\n" + "\n" + "\n" + "\n")
